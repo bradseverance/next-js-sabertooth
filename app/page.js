92d313467ddeb606470b73553a9b1e42cc225de3
Brad Severance <bseverance@SY-MAC-09-L.local>
2025-07-02 19:36:48 -0400

    Add last route for now.

--- app/page.js
+++ app/page.js
@@ -12,7 +12,7 @@ export default function Home() {
         <li><Link href="/shipping/orders-to-ship">Orders To Ship</Link></li>
         <li><Link href="/materials/bill-of-materials-viewer">Bill of Materials Viewer</Link></li>
         <li><Link href="/returns/returns-management">Returns Management</Link></li>
-        <li><Link href="">Print Labels</Link></li>
+        <li><Link href="/labels/print-labels">Print Labels</Link></li>
       </ul>
     </>
   );
